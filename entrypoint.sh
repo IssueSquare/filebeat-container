27fbf388ab2121ea2c6ca8692bd2111bf557d9dd
ArthurMa <4406arthur@gmail.com>
2017-06-14 13:14:05 +0800

    lost escape syntax in sed

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,12 @@
 
 #Set custom logpath
 if [ ! -z "$LOGPATH" ]; then
-   sed -i "s#- /var/log/*.log#- ${LOGPATH}/*.log#g" /tmp/filebeat.yml
+   sed -i "s#- \/var\/log\/\*\.log#- ${LOGPATH}/*.log#g" /tmp/filebeat.yml
 fi
 
 # Set custom elasticsearch endpoint
 if [ ! -z "$ELASTICHOST" ]; then
-   sed -i "s#hosts: ["localhost:9200"]#hosts: ["${ELASTICHOST}"]#g" /tmp/filebeat.yml
+   sed -i "s#hosts: \[\"localhost:9200\"\]#hosts: ["${ELASTICHOST}"]#g" /tmp/filebeat.yml
 fi
 
 # Set custom elasticsearch credential index
